f4ed04827fde777b0f720533eda7172caf6578c9
Reni Somoye <rensomoye@gmail.com>
2023-01-28 17:02:22 -0800

    commit tree f7e4f9ac24cd6995c619a3566cefc5524a99caf0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildTask(document.getElementById("new-task-description").value)
+  form.reset()
+})
 });
+
+function buildTask(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  p.textContent = `${task} `
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+  btn.addEventListener('click', deleteTask  )
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
